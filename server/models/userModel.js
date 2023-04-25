615e2bd33d52f878acb94579cb983a5683831ca8
Quyson Nguyen <Quyson.nguyen.1999@gmail.com>
2023-04-24 22:46:15 -0700

    Create Model Schema for User

---
+++ server/models/userModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  password: { type: String, required: true },
+  dob: { type: Date, required: true },
+  private: { type: Boolean, required: true },
+  profilePicture: { type: mongoose.Schema.Types.ObjectId, ref: "Image" },
+  sex: { type: String, enum: ["Male", "Female", "Else"], default: "Else" },
+  friendList: { type: Array, required: true, default: [] },
+  friendRequest: { type: Array, required: true, default: [] },
+});
+
+UserSchema.virtual("name").get(function () {
+  return `${this.first_name} ${this.last_name}`;
+});
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
---
+++ server/server.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const app = express.use();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const passport = require("passport");
