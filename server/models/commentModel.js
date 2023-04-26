401c6318a53aed93d5ce53f5867dafa05d1d7b81
Quyson Nguyen <Quyson.nguyen.1999@gmail.com>
2023-04-25 21:23:58 -0700

    Create Model Schema for Comments

--- server/models/commentModel.js
+++ server/models/commentModel.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const PostSchema = new Schema({
+const CommentSchema = new Schema({
   message: { type: String, required: true },
   likes: { type: Number, required: true, default: 0 },
+  likeList: { type: Array, required: true, default: [] },
   user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
-  likeLst: { type: Array, required: true, default: [] },
+  post: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Post" },
   picture: { type: mongoose.Schema.Types.ObjectId, ref: "Image" },
 });
 
-const Post = mongoose.model("Post", PostSchema);
-module.exports = Post;
+const Comment = mongoose.model("Comment", CommentSchema);
+module.exports = Comment;
