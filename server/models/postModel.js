a13af300975372212b38dd938904c96f48232794
Quyson Nguyen <Quyson.nguyen.1999@gmail.com>
2023-05-01 20:36:40 -0700

    Add directedTo attribute in Post Schema

--- server/models/postModel.js
+++ server/models/postModel.js
@@ -4,6 +4,11 @@ const Schema = mongoose.Schema;
 const PostSchema = new Schema({
   message: { type: String, required: true },
   likes: { type: Number, required: true, default: 0 },
+  directedTo: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
   user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
   likeLst: { type: Array, required: true, default: [] },
   picture: { type: mongoose.Schema.Types.ObjectId, ref: "Image" },
