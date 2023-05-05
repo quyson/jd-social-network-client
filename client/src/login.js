55e144510cee39ab8a9f0c2d967ceba4152bebed
Quyson Nguyen <Quyson.nguyen.1999@gmail.com>
2023-05-04 22:26:43 -0700

    Create login component

---
+++ client/src/login.js
@@ -0,0 +1,53 @@
+import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Navigate } from "react-router-dom";
+
+const Login = () => {
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [navigate, setNavigate] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await axios.post("http://localhost:8000/login", {
+      username: username,
+      password: password,
+    });
+    console.log(response.data.token);
+    localStorage.setItem("token", response.data.token);
+    setNavigate(true);
+  };
+
+  if (navigate) {
+    window.location.reload();
+    return <Navigate to={"/timeline"} />;
+  }
+  return (
+    <div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label for="username">Username</label>
+          <input
+            name="username"
+            id="username"
+            onChange={(e) => setUsername(e.target.value)}
+          ></input>
+          <label for="password">Password</label>
+          <input
+            name="password"
+            id="password"
+            type={"password"}
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
+          <button type="submit">Log In</button>
+        </form>
+      </div>
+      <div>
+        <a href="/signup">Sign up</a>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
