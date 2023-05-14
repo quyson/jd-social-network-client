004659662e5ec1deac630327a040340a8082cbae
Quyson Nguyen <Quyson.nguyen.1999@gmail.com>
2023-05-13 18:46:57 -0700

    Create redux configure store file

--- client/src/redux/configureStore.js
+++ client/src/redux/configureStore.js
@@ -1,15 +1,14 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userSlice from "./slices/userSlice";
+import tokenSlice from "./slicers/tokenSlice";
 
 const reducer = combineReducers({
-    currentUser: userSlice,
-    currentToken: 
-})
-
-const store = configureStore({
-	reducer: reducer
+  currentUser: userSlice,
+  currentToken: tokenSlice,
 });
 
+const store = configureStore({
+  reducer: reducer,
+});
 
-export default store
-
+export default store;
