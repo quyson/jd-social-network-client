5086f2e2872e4606670c02af27c2496470a18c26
Quyson Nguyen <Quyson.nguyen.1999@gmail.com>
2023-05-14 23:18:56 -0700

    Refactored logout component to include dispatch redux

--- client/src/logout.js
+++ client/src/logout.js
@@ -2,14 +2,18 @@ import React from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { setCurrentUser } from "./redux/slices/userSlice";
 
 const Logout = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleLogout = async (e) => {
     e.preventDefault();
     if (localStorage.getItem("token")) {
       localStorage.removeItem("token");
+      dispatch(setCurrentUser(null));
       navigate("/");
     } else {
       const result = await axios.post("http://localhost:8000/logout");
