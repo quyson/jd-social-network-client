44a9d2d3161dcacddce91b4dbca22f411cbd1102
Quyson Nguyen <Quyson.nguyen.1999@gmail.com>
2023-05-25 17:39:31 -0700

    Sticky navbar

--- client/src/navbar.js
+++ client/src/navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Logout from "./logout";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -8,16 +8,31 @@ import Notifications from "./notifications";
 const Navbar = () => {
   const [search, setSearch] = useState(null);
 
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset;
+      setIsScrolled(scrollTop > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   const currentUser = useSelector(
     (state) => state.user && state.user.currentUser
   );
 
-  console.log(currentUser);
-
   return (
     <div
-      className="fixed flex bg-stone-800 p-3 items-center 
-    justify-between h-16 border-b-2 border-neutral-500 w-full px-8"
+      className={`${
+        isScrolled ? "fixed top-0" : "sticky"
+      } flex bg-stone-800 -3 items-center 
+      justify-between h-16 border-b-2 border-neutral-500 w-full px-8`}
     >
       <h1 className="text-white font-bold text-xl">
         <Link to={`/timeline`}>JD SOCIAL NETWORK</Link>
