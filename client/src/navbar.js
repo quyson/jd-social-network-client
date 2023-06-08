c7c235da7c49b69aa2dd885479f160721a297854
Quyson Nguyen <Quyson.nguyen.1999@gmail.com>
2023-06-07 22:13:25 -0700

    Add bootstrap css to navbar

--- client/src/navbar.js
+++ client/src/navbar.js
@@ -29,33 +29,50 @@ const Navbar = () => {
   );
 
   return (
-    <nav className="container-fluid navbar fixed-top bg-dark">
-      <div className="row">
-        <h1 className="col">
-          <Link to={`/timeline`}>JD SOCIAL NETWORK</Link>
-        </h1>
-        <div>
-          <form>
+    <div className="container-fluid sticky-top">
+      <nav className="row p-2 navbar navbar-light bg-dark">
+        <div
+          className="col-4 d-flex align-items-center"
+          style={{ gap: "1rem" }}
+        >
+          <div
+            className="bg-primary rounded-circle text-light font-weight-bold d-flex justify-content-center align-items-center"
+            style={{ height: "3rem", width: "3rem", fontSize: "2rem" }}
+          >
+            JD
+          </div>
+          <h5 className="font-weight-bold">
+            <Link to={`/timeline`}>JD SOCIAL NETWORK</Link>
+          </h5>
+        </div>
+        <div className="col-4 d-flex justify-content-center align-items-center">
+          <form className="d-flex">
             <input
               id="search"
               name="search"
               placeholder="Search JD Network"
               onChange={(e) => setSearch(e.target.value)}
+              className="form-control"
             ></input>
             <Link to={`/search/${search}`}>
-              <button type="button">Search</button>
+              <button type="button" className="btn btn-primary rounded">
+                Search
+              </button>
             </Link>
           </form>
         </div>
-        <div>
+        <div
+          className="col-4 d-flex justify-content-center"
+          style={{ gap: "1rem" }}
+        >
           <Notifications />
           <Link to={`/profile`}>
             <div>{currentUser}</div>
           </Link>
           <Logout />
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 };
 
